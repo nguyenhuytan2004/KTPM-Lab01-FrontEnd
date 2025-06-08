af768386691197ee3f50d508802639363278208a
Tan <nguyenhuytan2004@gmail.com>
2025-06-08 10:46:49 +0700

    config for deploying (5)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Showtimes from "./pages/Showtimes";
-import Seats from "./pages/seats";
+import Seats from "./pages/Seats";
 
 const App = () => {
     const [username, setUsername] = useState("");
